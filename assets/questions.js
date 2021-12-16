4a38481203146fe73ac7b5deb3f54d6bbaf6e52e
alsornak <al.sorna01@gmail.com>
2021-12-16 23:13:09 +1100

    added the file

---
+++ assets/questions.js
@@ -0,0 +1,47 @@
+var questions = [
+    { 
+        question: "How do you write 'Hello World' in an alert box?", 
+        answers: [
+            { text: "msg('Hello World')", correct: false },
+            { text: "alert('Hello World')" , correct: true },
+            { text: "prompt('Hello World')", correct: false },
+            { text: "alertBox('Hello World')", correct: false }
+        ]
+    },
+    { 
+        question: "Which of the following function of Array object calls a function for each element in the array?", 
+        answers: [
+            { text: "concat()", correct: false },
+            { text: "filter()", correct: false },
+            { text: "forEach()", correct: true },
+            { text: "split()", correct: false }
+        ]
+    },
+    { 
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 8?", 
+        answers: [
+            { text: "if (i != 5)", correct: true },
+            { text: "if i =! 5", correct: false },
+            { text: "if (i <> 5)", correct: false },
+            { text: "if (i !=== 5)", correct: false }
+        ]
+    },
+    { 
+        question: "What is getItem commonly used for?", 
+        answers: [
+            { text: "naming a variable", correct: false },
+            { text: "local storage", correct: true },
+            { text: "arrarys", correct: false },
+            { text: "None of the above", correct: false }
+        ]
+    },
+    { 
+        question: "Arrays in Javascript can be used to store",
+        answers: [
+            { text: "object", correct: false },
+            { text: "strings", correct: false },
+            { text: "numbers", correct: false },
+            { text: "All of the above", correct: true }
+        ]
+    },
+];
